4aa7ede6037a1f20b176d8895473bcadb40be5b5
CedricNoiseux <cedric.noiseux@crim.ca>
2019-03-27 21:09:13 -0400

    Menu Layout

--- app/src/main/java/com/example/dnd/MainActivity.java
+++ app/src/main/java/com/example/dnd/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.dnd;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    private Button characters;
+    private Button sources;
+    private Button party;
+    private Button maps;
+    private Button monsters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set DnD font to buttons
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/DnDC.ttf");
+
+        // Characters button
+        characters = findViewById(R.id.character_button);
+        characters.setTypeface(custom_font);
+        characters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                beerList();
+            }
+        });
+
+        // Sources button
+        sources = findViewById(R.id.sources_button);
+        sources.setTypeface(custom_font);
+        sources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                procedure();
+            }
+        });
+
+        // Party button
+        party = findViewById(R.id.party_button);
+        party.setTypeface(custom_font);
+        party.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                tools();
+            }
+        });
+
+        // Maps button
+        maps = findViewById(R.id.maps_button);
+        maps.setTypeface(custom_font);
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                tools();
+            }
+        });
+
+        // Monstrers button
+        monsters = findViewById(R.id.monsters_button);
+        monsters.setTypeface(custom_font);
+        monsters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                tools();
+            }
+        });
+
     }
 }
